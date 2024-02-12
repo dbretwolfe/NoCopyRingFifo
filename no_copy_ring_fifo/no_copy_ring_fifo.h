d5b3ce80973d319cb3913b06e96b73c27aa75783
dbretwolfe <2679453+dbretwolfe@users.noreply.github.com~>
2024-02-11 23:49:39 -0800

    Added a comment describing overall class functionality. Cleaned up code comments. Renamed private class variables.

--- no_copy_ring_fifo/no_copy_ring_fifo.h
+++ no_copy_ring_fifo/no_copy_ring_fifo.h
@@ -1,3 +1,20 @@
+/*
+*   NoCopyRingFifo class
+*
+*   This class defines a type of FIFO that provides the user with Span objects that provide a readable and writable
+*   view into chunks of FIFO memory without copying data in or out.  This is useful when delegating the actual read and
+*   write operations for asynchronous/overlapped IO operations.
+*
+*   Writing to the FIFO happens in 3 stages - First a block of data in the FIFO is reserved, then at some later time
+*   data is written.  Once the write is complete, the written data is committed to the FIFO.
+*
+*   Once data is comitted to the FIFO, it is immediately available to read.
+*
+*   The underlying memory is a single contiguous block, and read and write operations wrap around the ends.  It is
+*   therefore possible that a read or write could involve two different copy operations on seperate sections of memory.
+*   The DataBlock class defined here contains two spans to cover the case of a wraparound.
+*/
+
 #pragma once
 
 #include <cstring>
@@ -27,87 +44,86 @@ public:
 
     NoCopyRingFifo(size_t size) : maxSize(size)
     {
-        ringBuffer.resize(size);
-        ringBufferSpan = std::span<T>(ringBuffer);
+        _ringBuffer.resize(size);
+        _ringBufferSpan = std::span<T>(_ringBuffer);
     }
 
     // Reserve a block of FIFO memory, returning a FifoBlock object.
     // An exception is thrown if there is insufficient reservable space.
-    DataBlock Reserve(size_t reserveSize)
+    DataBlock Reserve(size_t size)
     {
-        if (reserveSize > ReservableSize())
+        if (size > ReservableSize())
         {
             throw std::overflow_error(
                 std::format("Not enough free space in FIFO for reserve - requested {}, available {}",
-                reserveSize,
+                size,
                 ReservableSize())
                 );
         }
 
-        reserved += reserveSize;
+        _reserved += size;
 
-        return GetDataBlock(writeIndex, reserveSize);
+        return GetDataBlock(_writeIndex, size);
     }
 
     // Commit a block of data to the FIFO.  This increases the amount of committed data that is
     // available to be read and decreases the amount of reserved data, both by the commit size.
     // An exception is throw if there is insufficient reserved space for the commit.
-    void Commit(size_t commitSize)
+    void Commit(size_t size)
     {
-        if (commitSize > CommitableSize())
+        if (size > CommitableSize())
         {
             throw std::overflow_error(
                 std::format("Not enough reserved space in FIFO for commit - requested {}, available {}", 
-                commitSize,
+                size,
                 CommitableSize()
                 )
                 );
         }
 
-        committed += commitSize;
-        reserved -= commitSize;
+        _committed += size;
+        _reserved -= size;
     }
 
-    inline size_t ReservableSize(void) const { return (ringBuffer.size() - (reserved + committed)); }
-    inline size_t CommitableSize(void) const { return reserved; }
-    inline size_t ReadableSize(void) const { return committed; }
+    inline size_t ReservableSize(void) const { return (_ringBuffer.size() - (_reserved + _committed)); }
+    inline size_t CommitableSize(void) const { return _reserved; }
+    inline size_t ReadableSize(void) const { return _committed; }
 
-    // Get a span of data starting at the current read index.
-    // This does not increment the read index.
+    // Get a block of comitted data to read.
     DataBlock ReadBlock(size_t size)
     {
-        if (size > committed)
+        if (size > _committed)
         {
             throw std::underflow_error(
-                std::format("Read larger than committed size - requested {}, available {}", size, committed)
+                std::format("Read larger than committed size - requested {}, available {}", size, _committed)
                 );
         }
 
-        committed -= size;
+        _committed -= size;
 
-        return GetDataBlock(readIndex, size);
+        return GetDataBlock(_readIndex, size);
     }
 
     void Reset(void)
     {
-        readIndex = 0;
-        writeIndex = 0;
-        reserved = 0;
-        committed = 0;
+        _readIndex = 0;
+        _writeIndex = 0;
+        _reserved = 0;
+        _committed = 0;
     }
 
     const size_t maxSize;
     
 private:
-
+    // Get a block of data starting at the specified index.  This is used by both the Reserve and ReadBlock functions.
     DataBlock GetDataBlock(size_t& index, size_t size)
     {
-        if (size > ringBuffer.size())
+        if (size > _ringBuffer.size())
         {
             throw std::overflow_error(
                 std::format("Requested span size larger than FIFO size - requested {}, available {}", 
                 size,
-                ringBuffer.size()
+                _ringBuffer.size()
                 )
                 );
         }
@@ -116,27 +132,27 @@ private:
             return DataBlock();
         }
 
-        const size_t remainingBufferSize = (ringBuffer.size() - index);
+        const size_t remainingBufferSize = (_ringBuffer.size() - index);
         size_t oldIndex = index;
-        index = (index + size) % ringBuffer.size();
+        index = (index + size) % _ringBuffer.size();
 
         if (size > remainingBufferSize)
         {
             return DataBlock(
-                ringBufferSpan.subspan(oldIndex, remainingBufferSize),
-                ringBufferSpan.subspan(0, index)
+                _ringBufferSpan.subspan(oldIndex, remainingBufferSize),
+                _ringBufferSpan.subspan(0, index)
                 );
         }
         else
         {
-            return DataBlock(ringBufferSpan.subspan(oldIndex, size));
+            return DataBlock(_ringBufferSpan.subspan(oldIndex, size));
         }
     }
 
-    std::vector<T> ringBuffer;
-    std::span<T> ringBufferSpan;
-    size_t readIndex = 0;
-    size_t writeIndex = 0;
-    size_t reserved = 0;
-    size_t committed = 0;
+    std::vector<T> _ringBuffer;
+    std::span<T> _ringBufferSpan;
+    size_t _readIndex = 0;
+    size_t _writeIndex = 0;
+    size_t _reserved = 0;
+    size_t _committed = 0;
 };
